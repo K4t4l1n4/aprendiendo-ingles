1d248781c3f7426aeb575dad11f5228f659ce342
K4t4l1n4 <catalina.alsezu@gmail.com>
2026-05-29 23:22:24 -0400

    Agregar iconos PWA y corregir Service Worker

--- sw.js
+++ sw.js
@@ -1,8 +1,26 @@
-const CACHE = 'ingles-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'ingles-v2';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
